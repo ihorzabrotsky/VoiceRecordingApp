004451a6ff5aa6da183ccb4843465a464fd264c4
Ihor Zabrotsky <ihor.zabrotsky@gmail.com>
2025-11-23 13:00:30 +0200

    TODO added for audio player in AudioRecorderImpl1

--- VoiceRecordingApp/Data/AudioRecorderImpl1.swift
+++ VoiceRecordingApp/Data/AudioRecorderImpl1.swift
@@ -14,7 +14,7 @@ class AudioRecorderImpl1: AudioRecorder {
     static let shared = AudioRecorderImpl1() // TODO: shouldn't be Singleton. Must be correctly injected into Use Cases in future
     
     private var audioRecorder: AVAudioRecorder?
-    private var audioPlayer: AVAudioPlayer?
+    private var audioPlayer: AVAudioPlayer? // TODO: remove from here
     private var fileUrl: URL?
     private var isPaused: Bool = false
     
