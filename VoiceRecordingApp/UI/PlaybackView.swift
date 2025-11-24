9600d86f58cbec704ca48d0be80d83ed0c8624ae
Ihor Zabrotsky <ihor.zabrotsky@gmail.com>
2025-11-24 15:07:04 +0200

    Basic UI for PlaybackView

--- VoiceRecordingApp/UI/PlaybackView.swift
+++ VoiceRecordingApp/UI/PlaybackView.swift
@@ -9,16 +9,17 @@ import Foundation
 import SwiftUI
 import Combine
 
-enum PlaybackViewState {
-    case idle
-    case playing
-    case paused
+enum PlaybackViewState: String {
+    case idle = "Play"
+    case playing = "Pause"
+    case paused = "Resume"
 }
 
 final class PlaybackViewModel: ObservableObject {
     @Published var state: PlaybackViewState = .idle
     
-//    private let 
+    private let playRecordingUseCase = PlayRecordingUseCase()
+    
     
 }
 
@@ -26,6 +27,39 @@ struct PlaybackView: View {
     @StateObject private var viewModel = PlaybackViewModel()
     
     var body: some View {
-        Text("Playback View")
+        ZStack {
+            Text("Playback View")
+            
+            HStack {
+                // Play/Pause button
+                Button("\(viewModel.state.rawValue)") {
+                    switch viewModel.state {
+                    case .idle:
+                        viewModel.state = .playing
+                        
+                    case .playing:
+                        viewModel.state = .paused
+                        
+                    case .paused:
+                        viewModel.state = .playing
+                    }
+                    print("Play/Pause pressed")
+                }
+                .frame(width: 50, height: 50)
+                
+                Spacer()
+                
+                // Stop button
+                if viewModel.state != .idle {
+                    Button("Stop") {
+                        viewModel.state = .idle
+                        print("Stop button pressed")
+                    }
+                    .frame(width: 50, height: 50)
+                }
+            }
+            .frame(width: 120, height: 50)
+            .offset(y: 60)
+        }
     }
 }
